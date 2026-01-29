1c88a645c1fcf3098dc54e96763aa7dde10eb6b9
clairelincoln98 <clairelincoln98@gmail.com>
2026-01-29 18:41:10 -0500

    added new code

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-    
+using UnityEngine.Rendering.Universal;
+
 public class WASDController : MonoBehaviour
 { 
-    Rigidbody rb;
+    Rigidbody rb; //rigid body for the game object that this script is attached to
 
-    public float moveForce = 10f;
+    public float moveForce = 10f; //the force we will add to a game object to make it move
 
-    public Key keyUp = Key.W;
+    public Key keyUp = Key.W; //keyUp for the new input system
+    public  Key keyDown = Key.S; //keyDown for the new input system
+    public   Key keyLeft = Key.A; //keyLeft for the new input system
+    public   Key keyRight = Key.D; //keyRight for the new input system
 
-    Keyboard keyboard = Keyboard.current;
-    
+    Keyboard keyboard = Keyboard.current; //get the keyboard input for this device
+
+    private int integer;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); //searches for a component of this type on this gameobject
     }
 
     void FixedUpdate()
@@ -21,13 +26,29 @@ public class WASDController : MonoBehaviour
 
         if (keyboard[keyUp].isPressed)
         {
-            rb.AddForce(Vector3.up * moveForce, ForceMode.Acceleration);
+            rb.AddForce(Vector3.up * moveForce, ForceMode.Acceleration); //give the object an upward force
+        }
+        
+        if (keyboard[keyDown].isPressed)
+        {
+            rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration); //give the object an upward force
+        }
+        
+        if (keyboard[keyRight].isPressed)
+        {
+            rb.AddForce(Vector3.right * moveForce, ForceMode.Acceleration); //give the object an upward force
+        }
+        
+        if (keyboard[keyLeft].isPressed)
+        {
+            rb.AddForce(Vector3.right * moveForce, ForceMode.Acceleration); //give the object an upward force
         }
         
         
-        // if(Input.GetKey(KeyCode.S))
-        // {
-        //     rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration);
-        // }
+        
+     // if(Input.GetKey(KeyCode.S))
+    //    {
+     //  rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration); //give the object a downward force
+   //     }
     }
 }
